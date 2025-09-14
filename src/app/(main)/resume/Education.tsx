9ac77e1175fe184af8fe384ecaa26050727b20d4
unknown <mahmudhasan.hb@gmail.com>
2025-09-14 21:17:30 +0600

    completed education section

--- src/app/(main)/resume/Education.tsx
+++ src/app/(main)/resume/Education.tsx
@@ -5,19 +5,19 @@ import education from '@/assists/Education.json';
 const Education = () => {
     return (
         <section>
-            <h1 className='text-4xl font-bold'>Education Qualification</h1>
+            <h1 className='text-4xl font-bold mb-5'>Education Qualification</h1>
 
-            <div>
+            <div className='space-y-5'>
 
                 {
                     education.map((item, idx) =>
-                        <div className='space-y-2' key={idx}>
-                            <div>
+                        <div className='space-y-2 p-5 borderNew text-start' key={idx}>
+                            <div className='flex justify-between'>
                                 <div>
-                                    <h2 className='text-xl font-semibold'>{item.title}</h2>
+                                    <h2 className='text-2xl font-semibold'>{item.title}</h2>
                                     <p>{item.institution} <span>({item.startDate} - {item.endDate})</span></p>
                                 </div>
-                                <h1 className='bgcolor w-fit rounded-lg px-4 py-2 font-semibold'>{item.CGPA}/{item.outOf}</h1>
+                                <h1 className='bgcolor w-fit rounded-lg my-auto px-4 py-2 font-semibold'>{item.CGPA}/{item.outOf}</h1>
                             </div>
                             <p>{item.description}</p>
                         </div>
