28b5318ae0f9646fe2cdbed9886f216ac1fbba5f
unknown <mahmudhasan.hb@gmail.com>
2025-11-18 21:22:23 +0600

    completed meta section

--- src/app/(main)/portfolio/page.tsx
+++ src/app/(main)/portfolio/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import Portfolio from "./Portfolio";
 
+export const metadata: Metadata = {
+  title: "Portfolio - Mahmud Hasan Siddique",
+  description:
+    "Explore the portfolio of Mahmud Hasan Siddique, a Full Stack Developer specializing in React, Next.js, Node.js, Express, MongoDB, Prisma, and modern web application development. Skilled in building scalable APIs, responsive UIs, and complete end-to-end solutions.",
+};
 export default function Page() {
     return (
         <div>
--- src/app/(main)/resume/page.tsx
+++ src/app/(main)/resume/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
 import Resume from "./Resume";
+
+export const metadata: Metadata = {
+  title: "Resume - Mahmud Hasan Siddique",
+  description:
+    "Resume of Mahmud Hasan Siddique – Full Stack Developer experienced in React, Next.js, Node.js, Express, MongoDB, and Prisma. Includes professional experience, technical skills, education, and project achievements.",
+};
+
 
 export default function Page() {
     return (
