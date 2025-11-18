8c47e902f5a37f0c8bf25bba9bf75ca80ec2dbcf
unknown <mahmudhasan.hb@gmail.com>
2025-11-18 21:10:12 +0600

    completed category section

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -4,6 +4,7 @@ import Contact from "./Contact";
 export const metadata: Metadata = {
     title: "Contact Me - Mahmud Hasan Siddique",
     description: "Get in touch with Mahmud Hasan Siddique for collaborations, inquiries, or just to say hello. Full Stack Developer | React | Next | Node(MVC) | Express | MongoDB | Mongoose | Prisma | MySQL | JavaScript | Problem-solving.",
+    
 };
 
 
